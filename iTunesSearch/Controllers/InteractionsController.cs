7685671e916ba97398e4616ca2a3d0c3d405fddf
İlkin Elimov <e-ilkin@msn.com>
2020-09-05 23:11:30 -0700

    Display names are fixed. Unnecessary actions/views are removed.

--- iTunesSearch/Controllers/InteractionsController.cs
+++ iTunesSearch/Controllers/InteractionsController.cs
@@ -34,21 +34,6 @@ namespace iTunesSearch.Controllers
             return View(await interactions.ToListAsync());
         }
 
-        // GET: Interactions/Details/5
-        public async Task<ActionResult> Details(string id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Interaction interaction = await db.Interactions.FindAsync(id);
-            if (interaction == null)
-            {
-                return HttpNotFound();
-            }
-            return View(interaction);
-        }
-
         // GET: Interactions/Create
         public ActionResult Create()
         {
@@ -72,65 +57,6 @@ namespace iTunesSearch.Controllers
 
             ViewBag.UserId = new SelectList(db.Users, "Id", "Email", interaction.UserId);
             return View(interaction);
-        }
-
-        // GET: Interactions/Edit/5
-        public async Task<ActionResult> Edit(string id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Interaction interaction = await db.Interactions.FindAsync(id);
-            if (interaction == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.UserId = new SelectList(db.Users, "Id", "Email", interaction.UserId);
-            return View(interaction);
-        }
-
-        // POST: Interactions/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit([Bind(Include = "UserId,ClickedAdUrl,InteractionTime,DeviceName,ScreenSize")] Interaction interaction)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(interaction).State = EntityState.Modified;
-                await db.SaveChangesAsync();
-                return RedirectToAction("Index");
-            }
-            ViewBag.UserId = new SelectList(db.Users, "Id", "Email", interaction.UserId);
-            return View(interaction);
-        }
-
-        // GET: Interactions/Delete/5
-        public async Task<ActionResult> Delete(string id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Interaction interaction = await db.Interactions.FindAsync(id);
-            if (interaction == null)
-            {
-                return HttpNotFound();
-            }
-            return View(interaction);
-        }
-
-        // POST: Interactions/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> DeleteConfirmed(string id)
-        {
-            Interaction interaction = await db.Interactions.FindAsync(id);
-            db.Interactions.Remove(interaction);
-            await db.SaveChangesAsync();
-            return RedirectToAction("Index");
         }
 
         protected override void Dispose(bool disposing)
